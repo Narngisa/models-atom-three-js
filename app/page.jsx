2824994c0d6af636d2b327a811e251821e6478ee
Narngisa <Thaddark4826@gmail.com>
2024-06-18 23:01:39 +0700

    test

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className="bg-white h-screen">
+        <div className="text-center text-black font-mono font-semibold text-4xl p-8">
+          <h1>Models Atom</h1>
+        </div>
+        <div className='text-center text-black font-mono font-semibold text-2xl p-8'>
+          <div className='flex flex-col'>
+            <Link className='btn btn-outline btn-info' href="/pages/Rutherford">Rutherford</Link>
+            <Link className='btn btn-outline btn-success mt-4' href="/pages/Bohrv">Bohrv</Link>
+            <Link className='btn btn-outline btn-warning mt-4' href="/pages/Thomson">Thomson</Link>
+            <Link className='btn btn-outline btn-error mt-4' href="/pages/Fog-Group">Fog Group</Link>
+          </div>
+        </div>
+    </main>    
+  );
+}
---
+++ app/pages/Thomson/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three-stdlib";
+
+function page() {
+
+  const [gltf, setGltf] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+
+    const loader = new GLTFLoader();
+    loader.load("/thom/thomson.gltf", (gltf) => {
+      setGltf(gltf);
+    },
+    undefined,
+    (error) => {
+      console.error("An error happened", error);
+      setError(error);
+    }
+  );
+
+  }, []);
+
+  return (
+    <div className="bg-black h-[100vh]">
+      <Canvas>
+        <OrbitControls enableRotate autoRotate />
+        <ambientLight intensity={2} />
+        <directionalLight intensity={4} position={[-5, 5, 0]} />
+        {gltf && gltf.scene && <primitive object={gltf.scene} scale={2} />}
+      </Canvas>
+    </div>
+  );
+}
+
+export default page;
